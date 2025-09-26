f0f50b04cfe1d39b20bc6f5f9c82475b702a6c3a
shyam086 <patelshyan693@gmail.com>
2025-09-26 14:42:03 +0530

    Update LogoutButton.jsx

--- frontend/src/components/LogoutButton.jsx
+++ frontend/src/components/LogoutButton.jsx
@@ -12,7 +12,7 @@ const LogoutButton = () => {
   return (
     <button
       onClick={handleLogout}
-      className="mt-4 bg-purple-800 text-white px-4 py-2 rounded hover:bg-purple-900 transition"
+      className="mt-4 bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
     >
       Logout
     </button>
